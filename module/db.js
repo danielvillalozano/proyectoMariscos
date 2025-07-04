cc8d1ebff3f702eca420d955452f4b791149d918
Daniel Villa lozano <danielvillalozano6@gmail.com>
2025-07-03 18:30:17 -0700

    base de datos cambiada

--- module/db.js
+++ module/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: '127.0.0.1',
-  user: 'root',
-  password: 'ekisdeee123.',
+  user: 'admin',
+  password: '1234',
   database: 'proyectoMariscos',
   waitForConnections: true,
   connectionLimit: 10,
